c5a96c4f4c21c7fae877f7a8ef6611c39ca4ba56
Davi Slm <99913421+DaviSoaresLM@users.noreply.github.com>
2024-10-31 14:04:31 -0300

    atualização

--- src/main/java/com/minhaempresa/rede_vendas_api/data/dto/FornecedorDTO.java
+++ src/main/java/com/minhaempresa/rede_vendas_api/data/dto/FornecedorDTO.java
@@ -1,6 +1,14 @@
 package com.minhaempresa.rede_vendas_api.data.dto;
 
 
+import lombok.Data;
 
+@Data
 public class FornecedorDTO {
+
+    private Long Id;
+    private String nome;
+    private String contato;
+    private String email;
+    private String produtoOferecido;
 }
